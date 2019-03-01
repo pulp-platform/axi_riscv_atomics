43261aea3285a020c220b58eeda91c7481bb48ee
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-03-01 21:12:44 +0100

    axi_riscv_lrsc: Remove combinatorial loops

--- src/axi_riscv_lrsc.sv
+++ src/axi_riscv_lrsc.sv
@@ -449,7 +449,8 @@ module axi_riscv_lrsc #(
 
     // FIFO to track commands for W bursts.
     fifo_v3 #(
-        .FALL_THROUGH   (1'b1),
+        .FALL_THROUGH   (1'b0), // There would be a combinatorial loop if this were a fall-through
+                                // register.  Optimizing this can reduce the latency of this module.
         .dtype          (w_cmd_t),
         .DEPTH          (AXI_MAX_WRITE_TXNS)
     ) i_w_cmd_fifo (
@@ -864,13 +865,12 @@ module axi_riscv_lrsc #(
     );
 
     // Fall-through register in front of slv_r to remove mutual dependency.
-    fall_through_register #(
+    spill_register #( // There would be a combinatorial loop if this were a fall-through register.
+                      // Optimizing this can reduce the latency of this module.
         .T  (r_chan_t)
     ) slv_r_reg (
         .clk_i      (clk_i),
         .rst_ni     (rst_ni),
-        .clr_i      (1'b0),
-        .testmode_i (1'b0),
 
         .valid_i    (slv_r_valid),
         .ready_o    (slv_r_ready),
