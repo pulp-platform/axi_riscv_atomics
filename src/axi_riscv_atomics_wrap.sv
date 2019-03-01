a3af74cd3012f77cd21f0a1a7c9fd11c0825f926
Samuel Riedel <sriedel@student.ethz.ch>
2019-03-01 11:33:29 +0100

    axi_riscv_atomics_wrap: Add AXI_MAX_READ_TXNS parameter

--- src/axi_riscv_atomics_wrap.sv
+++ src/axi_riscv_atomics_wrap.sv
@@ -21,7 +21,9 @@ module axi_riscv_atomics_wrap #(
     parameter int unsigned AXI_DATA_WIDTH = 0,
     parameter int unsigned AXI_ID_WIDTH = 0,
     parameter int unsigned AXI_USER_WIDTH = 0,
-    /// Maximum number of AXI bursts outstanding at the same time
+    // Maximum number of AXI read bursts outstanding at the same time
+    parameter int unsigned AXI_MAX_READ_TXNS = 0,
+    // Maximum number of AXI write bursts outstanding at the same time
     parameter int unsigned AXI_MAX_WRITE_TXNS = 0,
     // Word width of the widest RISC-V processor that can issue requests to this module.
     // 32 for RV32; 64 for RV64, where both 32-bit (.W suffix) and 64-bit (.D suffix) AMOs are
@@ -41,6 +43,7 @@ module axi_riscv_atomics_wrap #(
         .AXI_DATA_WIDTH     (AXI_DATA_WIDTH),
         .AXI_ID_WIDTH       (AXI_ID_WIDTH),
         .AXI_USER_WIDTH     (AXI_USER_WIDTH),
+        .AXI_MAX_READ_TXNS  (AXI_MAX_READ_TXNS),
         .AXI_MAX_WRITE_TXNS (AXI_MAX_WRITE_TXNS),
         .RISCV_WORD_WIDTH   (RISCV_WORD_WIDTH)
     ) i_atomics (
