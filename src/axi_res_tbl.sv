817e27993f75bd58702f744084614aa902241cc8
Michael Rogenmoser <michael@rogenmoser.us>
2025-05-09 16:08:44 +0200

    Ensure unused entries get occupied before evicting used entries

--- src/axi_res_tbl.sv
+++ src/axi_res_tbl.sv
@@ -84,6 +84,7 @@ module axi_res_tbl #(
         logic [NUM_RESERVATIONS-1:0] plru_used, plru_evict_oh;
         logic [cf_math_pkg::idx_width(NUM_RESERVATIONS)-1:0] plru_evict;
         logic [NUM_RESERVATIONS-1:0][AXI_ID_WIDTH-1:0] tbl_id_d, tbl_id_q;
+        logic [NUM_RESERVATIONS-1:0] field_in_use_d, field_in_use_q;
 
         plru_tree #(
             .ENTRIES(NUM_RESERVATIONS)
@@ -105,26 +106,42 @@ module axi_res_tbl #(
             tbl_d = tbl_q;
             tbl_id_d = tbl_id_q;
             matching_set = 1'b0;
+            field_in_use_d = field_in_use_q;
             if (set) begin
+                // First reuse existing entry for ID
                 for (int i = 0; i < NUM_RESERVATIONS; ++i) begin
                     if (set_id_i == tbl_id_q[i]) begin
                         tbl_d[i] = set_addr_i;
                         plru_used[i] = 1'b1;
                         matching_set = 1'b1;
+                        field_in_use_d[i] = 1'b1;
                         break;
                     end
                 end
+                // Then check for unused entries
+                for (int i = 0; i < NUM_RESERVATIONS; ++i) begin
+                    if (!field_in_use_q[i] && !matching_set) begin
+                        tbl_d[i] = set_addr_i;
+                        plru_used[i] = 1'b1;
+                        matching_set = 1'b1;
+                        field_in_use_d[i] = 1'b1;
+                        break;
+                    end
+                end
+                // Finally evict the least recently used entry
                 for (int i = 0; i < NUM_RESERVATIONS; ++i) begin
                     if (i == plru_evict && !matching_set) begin
                         tbl_d[i] = set_addr_i;
                         tbl_id_d[i] = set_id_i;
                         plru_used[i] = 1'b1;
+                        field_in_use_d[i] = 1'b1;
                     end
                 end
             end else if (clr) begin
                 for (int i = 0; i < NUM_RESERVATIONS; ++i) begin
                     if (tbl_q[i] == check_clr_addr_i) begin
                         tbl_d[i] = '0;
+                        field_in_use_d[i] = 1'b0;
                     end
                 end
             end
@@ -141,7 +158,9 @@ module axi_res_tbl #(
 
             if (check_clr_req_i) begin
                 for (int i = 0; i < NUM_RESERVATIONS; i++) begin
-                    match |= (tbl_q[i] == check_clr_addr_i) && (tbl_id_q[i] == check_id_i);
+                    match |= (tbl_q[i] == check_clr_addr_i) &&
+                             (tbl_id_q[i] == check_id_i) &&
+                             field_in_use_q[i];
                 end
                 check_clr_gnt_o = 1'b1;
                 check_res_o     = match;
@@ -157,9 +176,11 @@ module axi_res_tbl #(
             if (~rst_ni) begin
                 tbl_q   <= '0;
                 tbl_id_q <= '0;
+                field_in_use_q <= '0;
             end else begin
                 tbl_q   <= tbl_d;
                 tbl_id_q <= tbl_id_d;
+                field_in_use_q <= field_in_use_d;
             end
         end
     end
